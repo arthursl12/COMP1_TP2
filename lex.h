91f94c074c29fc26b49ff288cd24c75600276319
arthursl12 <arthursoutolima@gmail.com>
2020-12-15 09:51:01 -0300

    Funções no arquivo Lex.h

--- lex.h
+++ lex.h
@@ -1,5 +1,16 @@
 #ifndef LEX_H
 #define LEX_H
 
+#include <string>
+
+bool isAddOp(std::string op);
+bool isMulOp(std::string op);
+bool isSpecOp(std::string op);
+bool isRelOp(std::string op);
+bool isOperator(std::string op);
+bool isDelimiter(std::string ch);
+bool isIdentifier(std::string str);
+std::string subString(std::string str, int left, int right);
+void findTokens(std::string program);
 
 #endif /* LEX_H */
