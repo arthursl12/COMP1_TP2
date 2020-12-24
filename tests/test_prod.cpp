f5899644c88d4c8373017953627aed52356f9446
arthursl12 <arthursoutolima@gmail.com>
2020-12-24 07:43:27 -0300

    Cadeia implementada

--- tests/test_prod.cpp
+++ tests/test_prod.cpp
@@ -4,36 +4,37 @@
 #include "doctest.h"
 
 #include <vector>
+#include <memory>
 
 TEST_CASE("Cadeia: construção"){
     SUBCASE("Construtor Simples"){
-        CHECK_NOTHROW(Cadeia(Terminal("")));
-        CHECK_THROWS(Cadeia());
+        std::shared_ptr<Symbol> p1 = std::make_shared<Terminal>("");
+        CHECK_NOTHROW(Cadeia(p1));
     }
     SUBCASE("Construir via vetor"){
-        std::vector<Symbol> v1;
-        v1.push_back(Terminal("a"));
-        v1.push_back(Terminal("b"));
-        v1.push_back(NaoTerminal("B1"));
-        v1.push_back(Terminal("b"));
-        v1.push_back(NaoTerminal("B1"));
-        v1.push_back(NaoTerminal("B1"));
+        std::vector<std::shared_ptr<Symbol>> v1;
+        v1.push_back(std::make_shared<Terminal>("a"));
+        v1.push_back(std::make_shared<Terminal>("b"));
+        v1.push_back(std::make_shared<NaoTerminal>("B1"));
+        v1.push_back(std::make_shared<Terminal>("b"));
+        v1.push_back(std::make_shared<NaoTerminal>("B1"));
+        v1.push_back(std::make_shared<NaoTerminal>("B1"));
         CHECK_NOTHROW(Cadeia(v1));
     }
 }
 
 TEST_CASE("Cadeia: operador []"){
-        std::vector<Symbol> v1;
-        v1.push_back(Terminal("a"));
-        v1.push_back(Terminal("b"));
-        v1.push_back(NaoTerminal("B1"));
-        v1.push_back(Terminal("b"));
-        v1.push_back(NaoTerminal("B1"));
-        v1.push_back(NaoTerminal("B1"));
+        std::vector<std::shared_ptr<Symbol>> v1;
+        v1.push_back(std::make_shared<Terminal>("a"));
+        v1.push_back(std::make_shared<Terminal>("b"));
+        v1.push_back(std::make_shared<NaoTerminal>("B1"));
+        v1.push_back(std::make_shared<Terminal>("b"));
+        v1.push_back(std::make_shared<NaoTerminal>("B1"));
+        v1.push_back(std::make_shared<NaoTerminal>("B1"));
         Cadeia c = Cadeia(v1);
 
         CHECK(c[0] == Terminal("a"));
         CHECK(c[1] == Terminal("b"));
         CHECK(c[5] == NaoTerminal("B1"));
-        CHECK_THROWS(c[6]);
+        CHECK_THROWS(c[10]);
 }
